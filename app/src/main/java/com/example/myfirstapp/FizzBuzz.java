0b3e178ea0f5df3872086b66765fd91250941d7b
Adam Brunner <BrunnerAdamJ@JohnDeere.com>
2021-03-02 15:21:14 -0600

    feat: add tests for multiples of 3, 5 and 3 * 5.

--- app/src/main/java/com/example/myfirstapp/FizzBuzz.java
+++ app/src/main/java/com/example/myfirstapp/FizzBuzz.java
@@ -2,9 +2,19 @@ package com.example.myfirstapp;
 
 public class FizzBuzz {
     public static String calc(int i) {
-        if(i == 3) {
+        if (i % 3 == 0 && i % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if(i % 3 == 0) {
             return "Fizz";
+        }
+        else if (i % 5 == 0) {
+            return "Buzz";
+        }
+        else if (i == 1){
+            return "1";
         }
         return null;
     }
+
 }
--- app/src/test/java/com/example/myfirstapp/FizzBuzzTest.java
+++ app/src/test/java/com/example/myfirstapp/FizzBuzzTest.java
@@ -8,23 +8,71 @@ public class FizzBuzzTest {
     @Test
     public void shouldReturnFizzFor3() {
         //Arrange or Given
-        int givenInput = 3;
+        assertMultipleOfThree(3);
+        assertMultipleOfThree(6);
+        assertMultipleOfThree(9);
+        assertMultipleOfThree(12);
+    }
 
+    private void assertMultipleOfThree(int givenInput) {
         //Act or When
         String returnVal = FizzBuzz.calc(givenInput);
 
         //Assert or Then
         assertEquals("Fizz", returnVal);
     }
+
     @Test
     public void shouldReturnBuzzFor5() {
         //Arrange or Given
-        int givenInput = 5;
+        assertMultipleOfFive(5 * 1);
+        assertMultipleOfFive(5 * 2);
+//        assertMultipleOfFive(5 * 3);
+        assertMultipleOfFive(5 * 4);
+    }
 
+    private void assertMultipleOfFive(int givenInput) {
         //Act or When
         String returnVal = FizzBuzz.calc(givenInput);
 
         //Assert or Then
         assertEquals("Buzz", returnVal);
     }
+
+    @Test
+    public void shouldReturnFizzBuzzFor15() {
+        //Arrange or Given
+        int givenInput = 5 * 3;
+
+        //Act or When
+        String returnVal = FizzBuzz.calc(givenInput);
+
+        //Assert or Then
+        assertEquals("FizzBuzz", returnVal);
+    }
+
+    @Test
+    public void shouldReturnFizzBuzzFor30() {
+        //Arrange or Given
+        int givenInput = 30;
+
+        //Act or When
+        String returnVal = FizzBuzz.calc(givenInput);
+
+        //Assert or Then
+        assertEquals("FizzBuzz", returnVal);
+    }
+
+    @Test
+    public void shouldReturnInput() {
+        //Arrange or Given
+        int givenInput = 1;
+
+        //Act or When
+        String returnVal = FizzBuzz.calc(givenInput);
+
+        //Assert or Then
+        assertEquals("" + givenInput, returnVal);
+    }
+
 }
